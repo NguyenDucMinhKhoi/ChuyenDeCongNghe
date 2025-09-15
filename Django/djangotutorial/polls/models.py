667b9de2522d4159452f519b754c1ec3f6110165
NguyenDucMinhKhoi <156874781+NguyenDucMinhKhoi@users.noreply.github.com>
2025-09-15 16:26:45 +0700

    model

--- Django/djangotutorial/polls/models.py
+++ Django/djangotutorial/polls/models.py
@@ -1,13 +1,12 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
 
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
     def __str__(self):
         return self.question_text
 
@@ -20,10 +19,10 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
